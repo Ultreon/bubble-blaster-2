04156a313355e9f706e87a18169c858e9fa68258
Qboi123 <qboiwastaken@gmail.com>
2022-10-22 01:37:09 +0200

    💼 Another test.

--- src/main/java/com/ultreon/dev/GameDevMain.java
+++ src/main/java/com/ultreon/dev/GameDevMain.java
@@ -1,24 +1,33 @@
 package com.ultreon.dev;
 
-import com.google.gson.Gson;
 import com.ultreon.preloader.PreGameLoader;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+@SuppressWarnings("ALL")
 public class GameDevMain {
     private static DevClassPath classPath;
     private static boolean isRanFromHere = false;
 
     public static void main(String[] args) {
         isRanFromHere = true;
-
-        final Gson gson = new Gson();
-
         System.setProperty("log4j2.formatMsgNoLookups", "true"); // Fix CVE-2021-44228 exploit.
-        String devClassPath = System.getenv("DEV_CLASS_PATH");
 
-        if (devClassPath == null) throw new NullPointerException("DEV_CLASS_PATH environment variable is absent.");
-
-        classPath = gson.fromJson(devClassPath, DevClassPath.class);
-        classPath.string(devClassPath);
+        final String keyStart = "com.ultreon.dev.sources.";
+        classPath = new DevClassPath();
+        Properties properties = System.getProperties();
+        properties.keySet().forEach(obj -> {
+            String key = obj.toString();
+            if (key.startsWith(keyStart)) {
+                String name = key.substring(keyStart.length());
+                Object o = properties.get(obj);
+                String classPath = o.toString();
+                String[] split = classPath.split("\\%s".formatted(System.getProperty("path.separator")));
+                GameDevMain.classPath.computeIfAbsent(key, s -> new ArrayList<>()).addAll(List.of(split));
+            }
+        });
 
         PreGameLoader.main(args);
     }
